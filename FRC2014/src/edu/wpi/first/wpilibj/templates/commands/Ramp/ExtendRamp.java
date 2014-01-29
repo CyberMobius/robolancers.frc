19caf9cebaca22035566795a474ef2311ed4550d
Armond Smith <xtalwings@gmail.com>
2014-01-29 17:33:19 -0500

    Merger of 1/29/2014

---
+++ FRC2014/src/edu/wpi/first/wpilibj/templates/commands/Ramp/ExtendRamp.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.commands.Ramp;
+
+import edu.wpi.first.wpilibj.templates.commands.CommandBase;
+
+/**
+ *
+ * @author RoboLancers
+ */
+public class ExtendRamp extends CommandBase{
+
+    private boolean hasFinished;
+    
+    protected void initialize() { }
+
+    protected void execute() { 
+        CommandBase.ramp.extend();
+    }
+
+    protected boolean isFinished() { return hasFinished; }
+
+    protected void end() { }
+
+    protected void interrupted() { }
+    
+}
